eb1aca186aae1b1c49585667e747e97916e8805b
NemesisUA <display.n0ne.01.gmail.com>
2025-04-28 15:23:43 +0300

    init: start project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <h1>Todo list in Junior, Middle and Senior style</h1>
+    </div>
+  )
+}
+
+export default App
