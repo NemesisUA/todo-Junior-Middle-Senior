e28ca65f615bc05e6322afa167bc0a6da9111e97
NemesisUA <display.n0ne.01.gmail.com>
2025-05-08 14:13:19 +0300

    run formatter

--- src/components/JuniorTodo.jsx
+++ src/components/JuniorTodo.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 
 export const JuniorTodo = () => {
-  const [todos, setTodos] = useState([{ title: 'Do something', id: 1, done: false }]);
+  const [todos, setTodos] = useState([
+    { title: 'Do something', id: 1, done: false },
+  ]);
   const [text, setText] = useState('');
 
   const handleAddTodo = () => {
     if (!text.trim()) {
-      return
+      return;
     }
 
     const newTodos = [...todos, { id: Date.now(), title: text, done: false }];
@@ -15,30 +17,45 @@ export const JuniorTodo = () => {
   };
 
   const handleDeleteTodo = (id) => {
-    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
-  }
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
 
   const toggleTodo = (id) => {
-    setTodos(prevTodos => {
-      return prevTodos.map(todo => todo.id === id ? { ...todo, done: !todo.done } : todo)
-    })
-  }
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      );
+    });
+  };
 
   return (
     <div className="section">
       <div>
         <h3>Add New Todo</h3>
-        <input className='new-todo' value={text} onChange={(e) => setText(e.target.value)} />
+        <input
+          className="new-todo"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <button onClick={handleAddTodo}>Add New Todo</button>
       </div>
 
       <h2>Todos List:</h2>
       <ul>
         {todos?.map((todo, index) => (
-          <li key={index} className='todo-item'>
-            <input type='checkbox' checked={todo.done} onChange={() => toggleTodo(todo.id)} />
+          <li key={index} className="todo-item">
+            <input
+              type="checkbox"
+              checked={todo.done}
+              onChange={() => toggleTodo(todo.id)}
+            />
             <span>{todo.title}</span>
-            <span className='trashkan' onClick={() => handleDeleteTodo(todo.id)}>x</span>
+            <span
+              className="trashkan"
+              onClick={() => handleDeleteTodo(todo.id)}
+            >
+              x
+            </span>
           </li>
         ))}
       </ul>
