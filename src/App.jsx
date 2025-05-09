c69868062ff137eb50726a5085938eb32a4e86df
NemesisUA <display.n0ne.01.gmail.com>
2025-05-09 14:52:23 +0300

    feat: simple todo with localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
+import { SeniorTodo } from './components/SeniorTodo';
 //import { JuniorTodo } from './components/JuniorTodo';
 //import { JuniorTodoFixMe } from './components/JuniorTodoFixMe';
-import { MiddleTodo } from './components/MiddleTodo';
+//import { MiddleTodo } from './components/MiddleTodo';
 
 function App() {
   return (
     <div className="app">
       <h1>Todo list in Junior, Middle and Senior style</h1>
 
-      <MiddleTodo />
+      <SeniorTodo />
     </div>
   );
 }
